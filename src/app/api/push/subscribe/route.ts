73b182370422973de62d8493e84a4dc923399f65
geisabitt <ageisislane@gmail.com>
2025-08-06 12:59:03 -0300

    update subscribe

--- src/app/api/push/subscribe/route.ts
+++ src/app/api/push/subscribe/route.ts
@@ -5,4 +5,5 @@ export async function POST(request: Request) {
   const subscription = await request.json();
   addSubscription(subscription);
   return NextResponse.json({ message: "Inscrito com sucesso!" });
+  // return new Response("Inscrito com sucesso!", { status: 201 });
 }
