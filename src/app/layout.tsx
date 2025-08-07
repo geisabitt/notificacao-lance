29f1c05cace73c914e684afabd43077a273617d1
geisabitt <ageisislane@gmail.com>
2025-08-06 23:04:19 -0300

    alterando o idioma do app pra pt-br

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <link rel="manifest" href="/manifest.json" />
       <meta name="theme-color" content="#000000" />
 
